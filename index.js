96f3e0db079e0260cec46b4eed99a9362789a046
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-06-15 00:14:18 +0300

    Modify my DOM with .id & textContent

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Cynthia Mugo is the champion";
